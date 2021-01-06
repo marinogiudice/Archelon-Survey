472c7cc3794ffcc01726e52335129f0ad138b03e
Marino Giudice <marino21183@gmail.com>
2021-01-06 01:06:56 +0000

    Grade file first set up - data binding enabled - UI Implementation
    - Fragments management dependencies
     added to the gradle file.
    - New UI Elements added to the build:
      Login Fragment
      Main Menu Fragment
      Morning Survey Screen 1 Fragment

---
+++ app/src/main/java/com/android/archelon/screens/morningsurvey/MorningSurveyFragment1.kt
@@ -0,0 +1,24 @@
+package com.android.archelon.screens.morningsurvey
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.android.archelon.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MorningSurveyFragment1 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_morning_survey1, container, false)
+    }
+
+}
