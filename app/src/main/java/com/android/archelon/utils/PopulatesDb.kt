aed3de3da955eb36aab8f524da68da1898e7cc97
Marino Giudice <marino21183@gmail.com>
2021-01-24 10:15:57 +0000

    Code comments added to the files and classes of the utils package
    Modified the email address for the dummy user account from .com to .gr

--- app/src/main/java/com/android/archelon/utils/PopulatesDb.kt
+++ app/src/main/java/com/android/archelon/utils/PopulatesDb.kt
@@ -2,24 +2,41 @@ package com.android.archelon.utils
 
 import com.android.archelon.dao.ArchelonDao
 import com.android.archelon.entities.*
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Dispatchers.IO
 
+/**
+ * Class PopulatesDb.
+ * It is an Utility Class.
+ * Contains methods and data used to populate the Db
+ * with Dummy data after creation.
+ * To modify the dummy data is possible to modify the lists arguments
+ * Takes the Dao as parameter
+ */
 class PopulatesDb(private var dao: ArchelonDao) {
-
+    //Contains the list of Dummy beaches
     private var beaches :List<String> = listOf("Mavrovouni","Selinitsa","Vathi","Valtaki")
+    //Contains the List of Dummy Sectors
     private var sectors :List<String> = listOf("North","South","East","West")
+    //Contains the list of dummy precipitation
     private var precipitation: List<String> = listOf("None","Rain","Hail","Snow")
+    //contains the list of sky condition
     private var sky:List<String> = listOf("Clear","Sunny","Cloudy")
+    //contains the list of wind intensity
     private var wind:List<String> = listOf("None","Breezy", "Windy")
-    private var user = User("volunteer1@archelon.com", "Volunteer1")
+    //contain the dummy user instance information
+    private var user = User("volunteer1@archelon.gr", "Volunteer1")
+    // contains the list of leaders
     private var leaders :List<String> = listOf("George Roussos", "Cosmin Stamate")
+    //contains the list of observers
     private var observers: List<String> = listOf("Marino Giudice", "Salik Tariq")
 
+    //use the dao to insert a user instance in the DB
     fun insertUser() {
         dao.insertUser(user)
     }
+
+    /*  Visit the List's elements, creates a Beach object for each element
+        and use the Dao to insert each object in the db */
+
     fun insertBeach() {
         for(i in 0..(beaches.size)-1) {
             var beach : Beach = Beach(beaches.get(i))
@@ -27,6 +44,8 @@ class PopulatesDb(private var dao: ArchelonDao) {
         }
     }
 
+    /*  Visit the List's elements, creates a BeachSector object for each element
+        and use the Dao to insert each object in the db */
     fun insertBeachSector() {
         for(i in 0..(sectors.size)-1) {
             var sector : BeachSector = BeachSector(sectors.get(i))
@@ -34,6 +53,8 @@ class PopulatesDb(private var dao: ArchelonDao) {
         }
     }
 
+    /*  Visit the List's elements, creates a Precipitation object for each element
+        and use the Dao to insert each object in the db */
     fun insertPrecipitation() {
         for(i in 0..(precipitation.size)-1) {
             var precipitation : Precipitation = Precipitation(precipitation.get(i))
@@ -41,6 +62,8 @@ class PopulatesDb(private var dao: ArchelonDao) {
         }
     }
 
+    /*  Visit the List's elements, creates a Sky object for each element
+        and use the Dao to insert each object in the db */
     fun insertSky() {
         for(i in 0..(sky.size)-1) {
             var sky : Sky = Sky(sky.get(i))
@@ -48,6 +71,8 @@ class PopulatesDb(private var dao: ArchelonDao) {
         }
     }
 
+    /*  Visit the List's elements, creates a Wind object for each element
+        and use the Dao to insert each object in the db */
     fun insertWind() {
         for (i in 0..(wind.size) - 1) {
             var wind: Wind = Wind(wind.get(i))
@@ -55,6 +80,8 @@ class PopulatesDb(private var dao: ArchelonDao) {
         }
     }
 
+    /*  Visit the List's elements, creates a Leaders object for each element
+        and use the Dao to insert each object in the db */
     fun insertLeaders() {
         for (i in 0..(leaders.size) - 1) {
             var leader: Leaders = Leaders(leaders.get(i))
@@ -62,6 +89,8 @@ class PopulatesDb(private var dao: ArchelonDao) {
         }
     }
 
+    /*  Visit the List's elements, creates a Observers object for each element
+        and use the Dao to insert each object in the db */
     fun insertObservers() {
         for (i in 0..(observers.size) - 1) {
             var observer: Observers = Observers(observers.get(i))
--- app/src/main/java/com/android/archelon/utils/utilities.kt
+++ app/src/main/java/com/android/archelon/utils/utilities.kt
@@ -3,11 +3,31 @@ package com.android.archelon.utils
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
+/**
+ * This file contains helper methods to validate the
+ * Username (email) and password format
+ * inserted by the user in the login screen
+ * I used google android developer website as pointer:
+ * https://developer.android.com/reference/java/util/regex/Pattern
+ */
+
+/*
+    takes a string as parameter
+    returns a boolean.
+    Uses the Pattern class java.util.regex
+    to check if the parameter string email contains a valid email address
+ */
 
 fun validateEmail(email: String ) : Boolean {
     return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
 }
 
+/*
+    takes a string as parameter
+    returns boolean
+    create a new Pattern object to define the valid character's set
+    check if the String pass parameter is validby the matcher object
+ */
 fun validatePassword(pass: String) : Boolean {
     val pattern: Pattern = Pattern.compile("[a-zA-Z0-9]{1,25}")
     val matcher: Matcher = pattern.matcher(pass)
