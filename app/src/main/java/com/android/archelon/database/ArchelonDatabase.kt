e9d2d6f2bf25eae1e9a6114576ebd73e2ee1d4fe
Marino Giudice <marino21183@gmail.com>
2021-01-25 00:50:12 +0000

    Code comments added for layout xml files and Database.kt

--- app/src/main/java/com/android/archelon/database/ArchelonDatabase.kt
+++ app/src/main/java/com/android/archelon/database/ArchelonDatabase.kt
@@ -11,15 +11,25 @@ import com.android.archelon.utils.PopulatesDb
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
-
+/**
+ * The ArchelonDatabase Class.
+ * Defines the RoomDatabase schema
+ * Uses a CallBack to populate the database on first creation.
+ * Implements the Singleton Pattern in the companion object to create a new Database if no found
+ * Returns the existing one otherwise.
+ * I used Google android developer website as pointer:
+ * https://developer.android.com/codelabs/android-room-with-a-view-kotlin#7
+ */
 @Database(entities = arrayOf(User::class,Beach::class,BeachSector::class, Precipitation::class, Sky::class, Wind::class, MorningSurvey::class, Leaders::class,Observers::class) , version = 1, exportSchema = false)
 abstract class ArchelonDatabase : RoomDatabase() {
 
     abstract fun archelonDao() : ArchelonDao
 
+    //defines a Room Callback
     private class ArchelonDatabaseCallback(
         private val scope: CoroutineScope
     ) : RoomDatabase.Callback() {
+        //defines the steps to populate the db on first creation
         override fun onCreate(db: SupportSQLiteDatabase) {
             super.onCreate(db)
             INSTANCE?.let { database ->
@@ -45,23 +55,19 @@ abstract class ArchelonDatabase : RoomDatabase() {
 
         @Volatile
         private var INSTANCE: ArchelonDatabase? = null
-
+        //implemets the singleton pattern
         fun getDatabase(context: Context,scope: CoroutineScope) : ArchelonDatabase {
-
+            //if a db instance is found
             if (INSTANCE != null) return INSTANCE!!
-
+            //if a db instance is not found
             synchronized(this) {
-
                 INSTANCE = Room
                     .databaseBuilder(context, ArchelonDatabase::class.java, "ARCHELON_DATABASE")
                     .addCallback(ArchelonDatabaseCallback(scope))
                     .allowMainThreadQueries()
                     .build()
-
                 return INSTANCE!!
-
             }
         }
-
     }
 }
